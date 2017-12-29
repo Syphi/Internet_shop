ab1b10c9a84fb4ccfc1d6ee98f3f07c8c171ba67
Syphi <ralkodenis@outlook.com>
2017-12-29 16:58:36 +0200

    final def orders_for_customer

--- DBlogic/sqlal_queries.py
+++ DBlogic/sqlal_queries.py
@@ -1,7 +1,6 @@
 import sqlalchemy as sa
 
 
-
 class DbSqlalQueries():
 
     def __init__(self):
@@ -73,7 +72,6 @@ class DbSqlalQueries():
         print(insert_list)
         self.connection.execute(insert, insert_list)
 
-
     def get_category(self):
         select = self.category.select()
         result = self.connection.execute(select).fetchall()
@@ -84,22 +82,63 @@ class DbSqlalQueries():
         join = self.category.join(self.category,
                                   category_id == self.category.c.parent_id)
         result = sa.select([self.category]).select_from(join)
-        print(result.fetchall())
-        return result
+        result_lst = self.connection.execute(result).fetchall()
+        print(result_lst)
+        return result_lst
 
     def products_for_category(self, category_id):
         select = self.product.select()
         select = select.where(self.product.c.category_id == category_id)
         return self.connection.execute(select).fetchall()
 
-    def orders_for_customer(self):
-        pass
+    def orders_for_customer(self, customer_id):
+        '''
+        need to give customer and order info
+        use 3 tables customer, orders, order_products
+
+        join with orders.id == orders_products.orders_id
+        :return: dict{{'Client_info': (id, user_name, email, password, phone, addres),
+        'Order_info': [(order.id, order.customer_id, order.sum_price, order.datetime, order.payment_type) ]}
+
+        '''
+        select = self.customer.select().\
+            where(self.customer.c.id == customer_id)
+        client_info = self.connection.execute(select).fetchall()[0]
+
+        result_dict = {'Client_info': client_info}
+
+        join = self.orders.join(self.order_products,
+                                self.orders.c.id == self.order_products.c.orders_id)
+        result = sa.select([self.orders, self.order_products]).select_from(join)
+        orders_lst = self.connection.execute(result).fetchall()
+
+        ids, orders_table = list(), list()
+        order_products = dict()
+        for order in orders_lst:
+            if order[0] in ids:
+                order_products[order[0]].append(order[6:])
+            else:
+                ids.append(order[0])
+                orders_table.append(order[:5])
+                order_products[order[0]] = []
+                order_products[order[0]].append(order[6:])
+
+        result_order_lst = []
+        for order in orders_table:
+            lst = []
+            print(order)
+            lst.append(order)
+            lst.append(order_products[order[0]])
+            result_order_lst.append(lst)
+
+        result_dict['Orders_info'] = result_order_lst
+        return result_dict
 
     def get_customer_from_login_and_password(self, login, password):
         select = self.customer.select().\
-            where(self.customer.c.user_name == sa.bindparam('login')).\
-            where(self.customer.c.password == sa.bindparam('password'))
-        return print(self.connection.execute(select, login=login, password=password).fetchall())
+            where(self.customer.c.user_name == sa.bindparam(login)).\
+            where(self.customer.c.password == sa.bindparam(password))
+        return self.connection.execute(select, login=login, password=password).fetchall()
 
 test = DbSqlalQueries()
 # test.add_category('name2', '//pass2//', '2description2', 'slug2', 1)
@@ -107,17 +146,25 @@ test = DbSqlalQueries()
 #                  price_ua=100, in_stock=25, other_info={})
 # test.add_customer(user_name='Denis2', email='2ralko2@gmail.com', password='2qwerty2',
 #                   phone='0671663238', shipping_address='Kovalski2')
-# test.add_orders(customer_id=1, sum_price=20, delivery_data_time='2017-12-19 10:23:54',
-#                 payment_type='card', dict={1: 2})
-# test.get_category()
+# test.add_orders(customer_id=1, sum_price=777, delivery_data_time='2018-10-19 10:23:54+02',
+#                 payment_type='cash', dict={1: 1, 5: 40})
+# test.get_category().sort(key=lambda x: x[0])
+test.get_subcategories(4)
 # test.products_for_category(1)
+# test.orders_for_customer(1)
+# print(test.get_customer_from_login_and_password('user', 'ralko96@gmail.com'))
 
 
-test.add_orders(customer_id=1, sum_price=20, delivery_data_time='2004-10-19 10:23:54+02',
-                payment_type='card', dict={1: 2, 5: 10})
+# Pass
+# add_category
+# add_product
+# add_customer
+# add_orders
+# get_category
+# products_for_category
+# orders_for_customer
 
 
-# try:
-#     test.get_customer_from_login_and_password('Denis', 'ralko@gmail.com')
-# except Exception as e:
-#     print(e)
+# Fall
+# get_subcategories
+# get_customer_from_login_and_password
